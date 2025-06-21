a0b420862d54bf93fde65bb9372015ff07fdeff3
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2025-06-20 22:34:04 -0500

    feat: :sparkles: pipelines

--- README.md
+++ README.md
@@ -69,5 +69,6 @@ va a variar la "coleccion" dependiendo la especificada y el "json" debe insertar
 
 ![alt text](image-6.png)
 ![alt text](image-7.png)
-
+![alt text](image-9.png)
+![alt text](image-10.png)
 ![alt text](image-8.png)
--- pipelines.js
+++ pipelines.js
@@ -7,10 +7,10 @@ db.ventas.aggregate([{ $unwind: "$productos" },{$group: {_id: "$productos.produc
 db.clientes.aggregate([{$project: {cantidadCompras: { $size: "$compras" }}},{$group: {_id: "$cantidadCompras",cantidadClientes: { $sum: 1 }}}])
   
 // 3. Mostrar el total de ventas por mes (usa `$group` y `$month`).
-
+db.ventas.aggregate([{$group: {_id: { $month: "$fecha" },totalVentas: { $sum: "$total" },cantidadVentas: { $sum: 1 },},},{$project: {_id: 0,mes: "$_id",totalVentas: 1,cantidadVentas: 1,},},{ $sort: { mes: 1 } },])
 
 // 4. Calcular el promedio de precios por categoría de producto.
-
+db.productos.aggregate([{$group: {  _id: "$categoria",precioPromedio: { $avg: "$precio" },cantidadProductos: { $sum: 1 },},},{$project: {_id: 0,categoria: "$_id",precioPromedio: { $round: ["$precioPromedio", 2] },cantidadProductos: 1,},},{ $sort: { precioPromedio: -1 } },])
 
 // 5. Mostrar los 3 productos con mayor stock (orden descendente con `$sort` y `$limit`).
 
